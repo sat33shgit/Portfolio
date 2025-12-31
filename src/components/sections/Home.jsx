a190c5ff74f96ea9a2588f02918d99df4946d079
sat33shgit <sateesh_b2000@yahoo.com>
2025-12-31 15:42:18 -0800

    Save: commit pending changes

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -61,7 +61,7 @@ export default function Home() {
                 <FaPaperPlane className="inline mr-2" /> Get in Touch
               </button>
               <a
-                href="https://drive.google.com/uc?export=download&id=1_DR1TiGp5p-Q5lu9MaUYqcUQW2Mu7uME"
+                href="https://drive.google.com/file/d/1wsnSMtT8Wboz0csURa2oOwRB1FZrpQ__/view?usp=sharing"
                 target="_blank"
                 rel="noopener noreferrer"
                 download
